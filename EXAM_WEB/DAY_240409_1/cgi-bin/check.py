4a3ad0aca52ed2dee1ffc19a8b0b1d2c1e4465ec
5amriley <jamesbyun1998@gmail.com>
2024-04-11 00:29:46 +0900

    2024/04/09 class content

---
+++ EXAM_WEB/DAY_240409_1/cgi-bin/check.py
@@ -0,0 +1,6 @@
+# HTML Header
+print('Content-Type: text/html')
+print() # 헤더와 바디 구분할 수 있게 하는 중요한 역할
+
+# HTML Body
+print('Hello, it\'s me')
---
+++ EXAM_WEB/DAY_240409_1/cgi-bin/index_01.py
@@ -0,0 +1,49 @@
+### 모듈 로딩
+import cgi
+import sys, codecs
+
+# Web 인코딩 설정 (한글 깨짐 해결)
+sys.stdout = codecs.getwriter('utf-8')(sys.stdout.detach())
+
+### 사용자 정의 함수
+### Web 브라우저 화면 출력 모드
+def print_browser(result=""):
+    # HTML 파일 읽기 -> body 문자열
+    filename = './html/test.html'   # DAY_240409_1(터미널 pwd) 기준 경로
+    with open(filename, mode='r', encoding='utf-8') as f:
+        # HTML Header
+        print('Content-Type: text/html')
+        print()
+        # HTML Body
+        print(f.read().format(result))
+
+
+## -----------------------------------------------
+## 요청 처리 및 브라우징
+## -----------------------------------------------
+
+### Client 요청 데이터, 즉, Form 데이터 저장 인스턴스
+form = cgi.FieldStorage()
+
+### 데이터 추출
+if 'data' in form and 'no' in form: # name 속성이 data 이거나 no
+    result = form.getvalue(key='data') +'-' + form.getvalue(key='no')
+else:
+    result = 'NO data'
+
+# if 'data' in form:
+#     result = form.getvalue(key='data')  # form['data']
+
+### 브라우징
+print_browser(result)
+
+
+# print('Content-Type: text/html')
+# print()
+
+# # HTML Body
+# print('<form><input type="text" name="data" placeholder="data"><br>')
+# print('<input type="number" name="no" placeholder="no"><br>')
+# print('<input type="submit"></form>')
+
+# print(f'Hello from the other side : {result}')
